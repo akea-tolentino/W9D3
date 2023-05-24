af812caf29955631f57b50578a72f2eaf15c2538
akea-tolentino <jamesbradley.tolentino@gmail.com>
2023-05-24 15:42:53 -0700

    defined follow/unfollow user functions

--- practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice/app/javascript/util/api.js
@@ -1,11 +1,25 @@
 const csrfToken = document.querySelector("meta[name=csrf-token]").content;
-export const foo = "bar";
 
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
     ...options.headers
   };
-
+  
   return await fetch(url, options);
 }
+
+ export const followUser = function (id) {
+  return customFetch(`/users/${id}/follow`, {
+    method: 'POST'
+  });
+}
+
+export const unfollowUser = function (id) {
+  return customFetch(`/users/${id}/follow`, {
+    method: 'DELETE'
+  });
+}
+
+// export const foo = "bar";
+
